ba272c8d48b4ea529715b2d9bd7363fa615d59d9
Ulysse <ulysse.aubanton@gmail.com>
2025-03-24 17:21:36 +0100

    Code most of the game
    I lost my good habit of committing regularly so I forgot... I followed
    the exercice until step 6 which gave me a hard time but I succeeded !
    I forgot many times the parenthesis for a function call.

--- script.js
+++ script.js
@@ -1,1 +1,69 @@
-console.log("gvcisdvi");
+function getComputerChoice () {
+    let randomNumber = Math.random()*100;
+    if (randomNumber < 33){
+        return "rock";
+    } else if (randomNumber > 66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+function getHumanChoice () {
+    return prompt("What's the shape of your hand, Human ?");
+}
+
+function playGame () {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound (humanChoice, computerChoice) {
+        console.log("The machine said "+computerChoice);
+        humanChoice = humanChoice.toLowerCase();
+    
+        if (humanChoice === computerChoice) {
+            console.log("Oh my god it's a tie !");
+        }
+        else if (humanChoice==="rock" && computerChoice==="scissors"){
+            console.log("You won against the machine !!");
+            ++humanScore;
+        }
+        else if (humanChoice==="scissors" && computerChoice==="paper"){
+            console.log("You won against the machine !!");
+            ++humanScore;
+        }
+        else if (humanChoice==="paper" && computerChoice==="rock"){
+            console.log("You won against the machine !!");
+            ++humanScore;
+        }
+        else {
+            console.log("You loose you fucking moron");
+            ++computerScore;
+        }
+    }
+
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+
+    return console.log("Your score is "+humanScore+" and the machine's is "+computerScore);
+}
+
+
+
