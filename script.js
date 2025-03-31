8092b4f7dbdfd88fa871aaa1a415711c0f29b83b
Ulysse <ulysse.aubanton@gmail.com>
2025-03-31 19:17:12 +0200

    Add event listeners to buttons and container div
    I struggled with adding the addEventListeners to the 3 buttons. In the
    end I used the for of loop to add to each one. Then I erased and
    replaced some of the code from the first time which used a lot of
    console.logs. I forgot to stop to this step and I started to add the div
    container for the result div to be appended.

--- script.js
+++ script.js
@@ -9,47 +9,41 @@ function getComputerChoice () {
     }
 }
 
-function getHumanChoice () {
-    return prompt("What's the shape of your hand, Human ?");
-}
 
-function playGame () {
-    let humanScore = 0;
-    let computerScore = 0;
 
-    function playRound (humanChoice, computerChoice) {
-        console.log("The machine said "+computerChoice);
-        humanChoice = humanChoice.toLowerCase();
-    
-        if (humanChoice === computerChoice) {
-            console.log("Oh my god it's a tie !");
-        }
-        else if (humanChoice==="rock" && computerChoice==="scissors"){
-            console.log("You won against the machine !!");
-            ++humanScore;
-        }
-        else if (humanChoice==="scissors" && computerChoice==="paper"){
-            console.log("You won against the machine !!");
-            ++humanScore;
-        }
-        else if (humanChoice==="paper" && computerChoice==="rock"){
-            console.log("You won against the machine !!");
-            ++humanScore;
-        }
-        else {
-            console.log("You loose you fucking moron");
-            ++computerScore;
-        }
+function playRound (humanChoice, computerChoice) {
+
+    if (humanChoice === computerChoice) {
+        console.log("Oh my god it's a tie !");
     }
-
-
-    // I think it would be better to just call the get functions directly into the playRound call
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
-    playRound(humanChoice, computerChoice);
-
-    return console.log("Your score is "+humanScore+" and the machine's is "+computerScore);
+    else if (humanChoice==="rock" && computerChoice==="scissors"){
+        console.log("You won against the machine !!");
+    }
+    else if (humanChoice==="scissors" && computerChoice==="paper"){
+        console.log("You won against the machine !!");
+    }
+    else if (humanChoice==="paper" && computerChoice==="rock"){
+        console.log("You won against the machine !!");
+    }
+    else {
+        console.log("You loose you fucking moron");
+    }
 }
 
-console.log("Hey welcome ! You can play my simple Shifumi game on the console: simply write the 'playGame();' command. Enjoy !")
+const buttons = document.querySelectorAll("button");
+for (btn of buttons) {
+    btn.addEventListener("click", (e)=> {
+        const playerSelection = e.target.className;
+        playRound(playerSelection, getComputerChoice());
+    })
+};
+
+const container = document.querySelector(".container");
+const resultDisplay = document.createElement("div");
+resultDisplay.textContent = "prout";
+container.appendChild(resultDisplay);
+
+
+
+
 
