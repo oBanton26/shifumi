991c55cc280309f0d9348cc2e38953176591c346
Ulysse <ulysse.aubanton@gmail.com>
2025-03-31 19:39:38 +0200

    Change display from console.log to DOM mods
    Like I said, I replaced the console.log methods with DOMs manipulations.
    The order of calling is important when appending the divs (because I
    also added a div to tell the choices from user and computer).

--- script.js
+++ script.js
@@ -12,25 +12,36 @@ function getComputerChoice () {
 
 
 function playRound (humanChoice, computerChoice) {
-
+    
+    roundChoices.textContent = `You played ${humanChoice} and the machine ${computerChoice}`;
+    
     if (humanChoice === computerChoice) {
-        console.log("Oh my god it's a tie !");
+        resultDisplay.textContent = "Oh my god it's a tie !";
     }
     else if (humanChoice==="rock" && computerChoice==="scissors"){
-        console.log("You won against the machine !!");
+        resultDisplay.textContent = "You won against the machine !";
     }
     else if (humanChoice==="scissors" && computerChoice==="paper"){
-        console.log("You won against the machine !!");
+        resultDisplay.textContent = "You won against the machine !";
     }
     else if (humanChoice==="paper" && computerChoice==="rock"){
-        console.log("You won against the machine !!");
+        resultDisplay.textContent = "You won against the machine !";
     }
     else {
-        console.log("You loose you fucking moron");
+        resultDisplay.textContent = "You loose, you fucking moron...";
     }
+
+    
 }
 
+const container = document.querySelector(".container");
+const roundChoices = document.createElement("div");
+const resultDisplay = document.createElement("div");
+
+container.appendChild(roundChoices);
+container.appendChild(resultDisplay);
 const buttons = document.querySelectorAll("button");
+
 for (btn of buttons) {
     btn.addEventListener("click", (e)=> {
         const playerSelection = e.target.className;
@@ -38,10 +49,8 @@ for (btn of buttons) {
     })
 };
 
-const container = document.querySelector(".container");
-const resultDisplay = document.createElement("div");
-resultDisplay.textContent = "prout";
-container.appendChild(resultDisplay);
+
+
 
 
 
