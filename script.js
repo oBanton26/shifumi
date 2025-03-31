da4c54e7d374ae6577e54aba1fe12212c84f988e
Ulysse <ulysse.aubanton@gmail.com>
2025-03-31 20:34:19 +0200

    Add score and winner message

--- script.js
+++ script.js
@@ -9,38 +9,54 @@ function getComputerChoice () {
     }
 }
 
-
+let computerScore = 0;
+let humanScore = 0;
 
 function playRound (humanChoice, computerChoice) {
     
-    roundChoices.textContent = `You played ${humanChoice} and the machine ${computerChoice}`;
+    roundChoices.textContent = `You played ${humanChoice} and the machine ${computerChoice}.`;
     
     if (humanChoice === computerChoice) {
         resultDisplay.textContent = "Oh my god it's a tie !";
+        
     }
     else if (humanChoice==="rock" && computerChoice==="scissors"){
         resultDisplay.textContent = "You won against the machine !";
+        humanScore++;
     }
     else if (humanChoice==="scissors" && computerChoice==="paper"){
         resultDisplay.textContent = "You won against the machine !";
+        humanScore++;
     }
     else if (humanChoice==="paper" && computerChoice==="rock"){
         resultDisplay.textContent = "You won against the machine !";
+        humanScore++;
     }
     else {
         resultDisplay.textContent = "You loose, you fucking moron...";
+        computerScore++;
     }
 
-    
+    score.textContent = `Human: ${humanScore} ${computerScore} :Computer`;
+
+    if (humanScore == 5 || computerScore == 5) {
+        score.textContent = `THE WINNER IS ${(humanScore>computerScore)?"YOU !":"THE COMPUTER !"}`
+    }
 }
 
+// References
 const container = document.querySelector(".container");
 const roundChoices = document.createElement("div");
 const resultDisplay = document.createElement("div");
+const buttons = document.querySelectorAll("button");
+
+const score = document.createElement("div");
+
 
 container.appendChild(roundChoices);
 container.appendChild(resultDisplay);
-const buttons = document.querySelectorAll("button");
+container.appendChild(score);
+
 
 for (btn of buttons) {
     btn.addEventListener("click", (e)=> {
